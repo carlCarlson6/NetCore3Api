e6f2f7ce0addd29d0766d7a85730f033cf706c72
carlCarlson6 <bullet.carlos@gmail.com>
2020-08-02 17:36:34 +0200

    finish coded core layer

---
+++ MyMusic/Core/Services/IArtistService.cs
@@ -0,0 +1,15 @@
+﻿using Core.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Core.Services
+{
+    public interface IArtistService
+    {
+        Task<IEnumerable<Artist>> GetAllArtist();
+        Task<Artist> GetArtistById(int id);
+        Task<Music> CreateArtist(Artist newArtist);
+        Task UpdateArtist(Artist artistToBeUpdated, Artist artist);
+        Task DeleteArtist(Artist artist);
+    }
+}
