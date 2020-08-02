80fe76414999a87bac2807a6908a58042b7e7c11
carlCarlson6 <bullet.carlos@gmail.com>
2020-08-02 20:56:25 +0200

    more coding

--- MyMusic/Api/Controllers/MusicController.cs
+++ MyMusic/Api/Controllers/MusicController.cs
@@ -1,4 +1,5 @@
-﻿using AutoMapper;
+﻿using Api.Resources;
+using AutoMapper;
 using Core.Models;
 using Core.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -9,19 +10,33 @@ using System.Threading.Tasks;
 
 namespace Api.Controllers
 {
-    [Route("api/Musics")]
+    [Route("api/[controller]")]
     [ApiController]
     public class MusicController : ControllerBase
     {
         private readonly IMusicService musicService;
+        private readonly IMapper mapper;
 
-        public MusicController(IMusicService musicService, IMapper mapper) => this.musicService = musicService;
+        public MusicController(IMusicService musicService, IMapper mapper)
+        {
+            this.musicService = musicService;
+            this.mapper = mapper;
+        }
 
         [HttpGet("")]
-        public async Task<ActionResult<IEnumerable<Music>>> GetAllMusics()
+        public async Task<ActionResult<IEnumerable<MusicResource>>> GetAllMusics()
         {
             IEnumerable<Music> musics = await this.musicService.GetAllWithArtist();
-            return Ok(musics);
+            IEnumerable<MusicResource> musicResources = this.mapper.Map<IEnumerable<Music>, IEnumerable<MusicResource>>(musics);
+            return Ok(musicResources);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<MusicResource>> GetMusicById(int id)
+        {
+            Music music = await this.musicService.GetMusicById(id);
+            MusicResource musicResource = this.mapper.Map<Music, MusicResource>(music);
+            return Ok(musicResource);
         }
     }
 }
---
+++ MyMusic/Api/Mapping/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using Api.Resources;
+using AutoMapper;
+using Core.Models;
+
+namespace Api.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // domain to resource
+            CreateMap<Music, MusicResource>();
+            CreateMap<Artist, ArtistResource>();
+
+            // resource to domain
+            CreateMap<MusicResource, Music>();
+            CreateMap<ArtistResource, Artist>();
+        }
+    }
+}
